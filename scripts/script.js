d2296501011162e5361273e40bbb7abe81638b56
Justin Winistoerfer <winistoerfer.justin@gmx.ch>
2023-04-28 12:50:22 +0200

    Some changes

--- scripts/script.js
+++ scripts/script.js
@@ -4,7 +4,7 @@ new Glide('.glide', {
     type: 'carousel',
     perView: 1,
     autoplay: 4000,
-    animationDuration: 1000
+    animationDuration: 600
 }).mount()
 
 window.addEventListener("load", (event) => {
